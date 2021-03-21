3386cb9809dc68801f539a1b472d5cb2dee38712
forderation <kharisma.muzaki@gmail.com>
2021-03-21 21:13:15 +0700

    name upt

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-        <title>Simagang - UPT PLN Pasuruan</title>
+        <title>Sistem Pengajuan Magang - PLN Pasuruan</title>
         <meta name="author" content="pixelcave">
         <meta name="robots" content="noindex, nofollow">
 
